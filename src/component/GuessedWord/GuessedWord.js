82caad4d5b59755a7b44e1ea60f83e1b453e850b
captainamiedi <captainamiedi1@.com>
2019-08-07 16:23:55 +0100

    Add test and code GuessWord component when there is no guess

--- src/component/GuessedWord/GuessedWord.js
+++ src/component/GuessedWord/GuessedWord.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const guessWordTable = () => {
-  return <div />;
-};
-
 export function GuessedWord(prop) {
   const { guessedWord } = prop;
-  const guessWordInstruction = <h2 data-test='guessWord-instruction'>Try to guess the secret word</h2>
+  const guessWordInstruction = <h2 data-test='guessWord-instruction'>Try to guess the secret word</h2>;
+  const guessWordTable = (
+    <div data-test='guessWord-div'>
+      <table>
+        <thead>
+          <tr><th>Guess</th><th>Matching Letters</th></tr>
+        </thead>
+        <tbody>
+          {guessedWord.map((word, index) => (
+            <tr data-test='guessWord' key={index}>
+              <td>{word.guess}</td>
+              <td>{word.letterMatchCount}</td>
+            </tr>)
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+
   return (
     <div data-test='guessWord-container'>
       {(guessedWord.length === 0 ? guessWordInstruction : guessWordTable)}
--- src/component/GuessedWord/GuessedWord.test.js
+++ src/component/GuessedWord/GuessedWord.test.js
@@ -28,5 +28,25 @@ describe('GuessedWord Component with no guessed word', () => {
 });
 
 describe('GuessWord Component with some guessedWord', () => {
-
+  let wrapper;
+  const guessedWord = [
+    { guess: 'train', letterMatchCount: 3 },
+    { guess: 'drunk', letterMatchCount: 1 },
+    { guess: 'party', letterMatchCount: 5 }
+  ];
+  beforeEach(() => {
+    wrapper = setup({ guessedWord });
+  });
+  it('should render without errors', () => {
+    const guessWordContainer = findTestByAttr(wrapper, 'guessWord-container');
+    expect(guessWordContainer.length).toBe(1);
+  });
+  it('should render GuessWord Section', () => {
+    const guessWordDiv = findTestByAttr(wrapper, 'guessWord-div')
+    expect(guessWordDiv.length).toBe(1);
+  });
+  it('should return the guessedWord and the letterMatchCount', () => {
+    const guessWordNode = findTestByAttr(wrapper, 'guessWord')
+    expect(guessWordNode.length).toBe(guessedWord.length)
+  })
 })
