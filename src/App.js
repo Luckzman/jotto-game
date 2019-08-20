874468c232c7b61c1a89415907cf12906b2c3874
Lucky <lumpsey@gmail.com>
2019-08-20 23:44:52 +0100

    refactor user secret word into a separate component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Congrats from './component/Congrats';
 import GuessedWord from './component/GuessedWord';
 import Input from './component/Input';
 import ServerError from './component/ServerError';
+import SecretWordInput from './component/SecretWordInput';
 import { getSecretWord, newGame } from './actions'
 
 import './App.css';
@@ -36,13 +37,13 @@ export class UnconnectedApp extends Component {
     this.setState({ isHidden: true })
   }
 
-  createSecretWord = (evt) => {
-    evt.preventDefault()
-    this.props.getSecretWord(this.inputBox.current.value);
-    this.inputBox.current.value = '';
-    this.setState({ isHidden: false })
+  // createSecretWord = (evt) => {
+  //   evt.preventDefault()
+  //   this.props.getSecretWord(this.inputBox.current.value);
+  //   this.inputBox.current.value = '';
+  //   this.setState({ isHidden: false })
 
-  }
+  // }
 
   render() {
     const { success, guessedWords, error } = this.props;
@@ -51,39 +52,25 @@ export class UnconnectedApp extends Component {
     return (
       <div className="container">
         <h1>Jotto Game</h1>
-        {error && <ServerError />}
-        {!error && (isHidden ? (
-          <form>
-            <p>Enter a secret word for someone to guess</p>
-            <input
-              className="mb-2 mx-sm-3"
-              ref={this.inputBox}
-            />
-            <button
-              className="btn btn-success ml-3"
-              onClick={this.createSecretWord}
-            >
-              Submit
+        {error ? <ServerError /> :
+          (isHidden ? <SecretWordInput hideInput={() => this.setState({ isHidden: false })} />
+            :
+            <React.Fragment>
+              <Congrats success={success} />
+              {success && <NewWord createNewGame={this.handleNewGame} />}
+              <Input />
+              <GuessedWord guessedWords={guessedWords} />
+              {guessedWords.length > 0 && <TotalGuess totalGuessNumber={guessedWords.length} />}
+              {guessedWords.length === 0 && (
+                <button
+                  className="btn btn-primary mt-5"
+                  onClick={this.inputSecretWord}
+                >
+                  Enter your own secret word
               </button>
-          </form>
-        ) :
-          <React.Fragment>
-            <Congrats success={success} />
-            {success && <NewWord createNewGame={this.handleNewGame} />}
-            <Input />
-            <GuessedWord guessedWords={guessedWords} />
-            {guessedWords.length > 0 && <TotalGuess totalGuessNumber={guessedWords.length} />}
-            {guessedWords.length === 0 && (
-              <button
-                className="btn btn-primary mt-5"
-                onClick={this.inputSecretWord}
-              >
-                Enter your own secret word
-            </button>
-            )}
-          </React.Fragment>
-        )}
-
+              )}
+            </React.Fragment>
+          )}
       </div>
     );
   }
---
+++ src/component/SecretWordInput/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSecretWord } from '../../actions';
+
+export class SecretWordInput extends Component {
+
+  state = {
+    secret: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({ secret: event.target.value })
+  }
+
+  createSecretWord = (evt) => {
+    evt.preventDefault()
+    const { secret } = this.state;
+    this.props.getSecretWord(secret);
+    this.props.hideInput()
+  }
+
+  render() {
+    const { secret } = this.state;
+
+    return (
+      <form>
+        <p>Enter a secret word for someone to guess</p>
+        <input
+          className="mb-2 mx-sm-3"
+          value={secret}
+          onChange={this.handleChange}
+        />
+        <button
+          className="btn btn-success ml-3"
+          onClick={this.createSecretWord}>
+          Submit
+        </button>
+      </form>
+    )
+  }
+}
+
+export default connect(null, { getSecretWord })(SecretWordInput);
